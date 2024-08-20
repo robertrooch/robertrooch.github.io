c7bacdffd95ba2480f0d4b69fa8158f67510d8b9
robertrooch <robertorooch2004@gmail.com>
2024-08-20 12:27:04 -0600

    create base code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mycv');
+    expect(compiled.querySelector('b')?.textContent).toContain('Roberto Rojas Ochoa');
   });
 });
