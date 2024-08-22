53ed6be461ac85a96fe7271f812536175a6f857e
robertrooch <robertorooch2004@gmail.com>
2024-08-22 07:27:20 -0600

    update title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('b')?.textContent).toContain('Roberto Rojas Ochoa');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Roberto Rojas Ochoa');
   });
 });
