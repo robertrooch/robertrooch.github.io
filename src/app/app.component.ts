f96f142b96cdde3f5476f0e7c22982d1aeebdc84
robertrooch <146406947+robertrooch@users.noreply.github.com>
2024-09-21 20:34:45 -0600

    Update app.component.ts (#31)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
-
+import { LenguagesComponent } from './lenguages/lenguages.component';
+import { InterestsComponent } from './interests/interests.component';
+import { ArchievementsComponent } from './archievements/archievements.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, EducationComponent, SkillsComponent],
-
+  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, EducationComponent, SkillsComponent, LenguagesComponent, InterestsComponent, ArchievementsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
